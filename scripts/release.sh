7ee2a07a131fb3c0ab6e04ed4cff509e9fc1226b
HugoRCD <hugo.richard@epitech.eu>
2024-04-15 20:59:14 +0200

    add main release ci

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Restore all git changes
+git restore -s@ -SW  -- .
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release package
+echo "Publishing mockline"
+npm publish -q --access public
