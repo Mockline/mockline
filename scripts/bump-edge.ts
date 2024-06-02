fd359338a8d282cb3a5ae6d990d012a5e84034be
HugoRCD <hugo.richard@epitech.eu>
2024-06-02 22:28:25 +0200

    Add old bump-edge

--- scripts/bump-edge.ts
+++ scripts/bump-edge.ts
@@ -1,17 +1,14 @@
 import { promises as fsp } from 'fs'
 import { resolve } from 'path'
 import { execSync } from 'child_process'
+import { join } from 'pathe'
 
-async function loadPackage(dir: string): Promise<{
-  dir: string
-  data: Record<string, any>
-  save: () => Promise<void>
-}> {
+async function loadPackage(dir: string) {
   const pkgPath = resolve(dir, 'package.json')
 
   const data = JSON.parse(await fsp.readFile(pkgPath, 'utf-8').catch(() => '{}'))
 
-  const save = (): Promise<void> => fsp.writeFile(pkgPath, JSON.stringify(data, null, 2) + '\n')
+  const save = () => fsp.writeFile(pkgPath, JSON.stringify(data, null, 2) + '\n')
 
   return {
     dir,
@@ -20,9 +17,9 @@ async function loadPackage(dir: string): Promise<{
   }
 }
 
-async function main(): Promise<void> {
-  // load package.json from current working directory / repo directory (root) located in "~/packages/mockline", this script is located in "~/scripts/bump-edge.ts"
-  const pkg = await loadPackage(resolve(__dirname, '../packages/mockline'))
+async function main() {
+  const rootDir = resolve(__dirname, '..')
+  const pkg = await loadPackage(join(rootDir, 'packages', 'mockline'))
 
   const commit = execSync('git rev-parse --short HEAD').toString('utf-8').trim()
 
@@ -36,6 +33,7 @@ async function main(): Promise<void> {
 }
 
 main().catch((err) => {
+  // eslint-disable-next-line no-console
   console.error(err)
   process.exit(1)
 })
