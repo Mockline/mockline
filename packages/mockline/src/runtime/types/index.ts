6d6a035b4f56ddd37fa5ec2627d8090e88cf19f2
HugoRCD <hugo.richard@epitech.eu>
2024-10-14 22:21:58 +0200

    Add types index

---
+++ packages/mockline/src/runtime/types/index.ts
@@ -0,0 +1,31 @@
+// Elements
+export * from '../components/elements/Button.vue'
+export * from '../components/elements/NavMenu.vue'
+
+// Layout
+export * from '../components/layout/App.vue'
+export * from '../components/layout/Aside.vue'
+export * from '../components/layout/Card.vue'
+export * from '../components/layout/Header.vue'
+export * from '../components/layout/Main.vue'
+export * from '../components/layout/Page.vue'
+export * from '../components/layout/PageBody.vue'
+export * from '../components/layout/PageHeader.vue'
+
+// Settings
+export * from '../components/settings/ThemeToggle.vue'
+
+// Overlays
+export * from '../components/overlays/Toasts.vue'
+
+// Content
+export * from '../components/content/ContentNavigationTree.vue'
+export * from '../components/content/ContentSurround.vue'
+export * from '../components/content/ContentToc.vue'
+export * from '../components/content/ContentTocLinks.vue'
+
+// Global
+export * from '../components/global/Callout.vue'
+export * from '../components/global/CodeGroup.vue'
+export * from '../components/global/prose/ProseCode.vue'
+export * from '../components/global/prose/ProseCodeButton.vue'
