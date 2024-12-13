7a5f77f61733f1b3e0556e15d3e446eb91bf1290
johann <johann.cavallucci@epitech.eu>
2024-12-13 10:10:58 +0100

    feat: Switch Loading fixed

--- packages/themes/src/components/switch.ts
+++ packages/themes/src/components/switch.ts
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 export const switchTv = tv({
   slots: {
-    base: 'w-[32px] h-[20px] shadow-sm flex data-[state=unchecked]:bg-stone-300 data-[state=checked]:bg-stone-800 dark:data-[state=unchecked]:bg-stone-800 dark:data-[state=checked]:bg-stone-700 border border-stone-300 data-[state=checked]:border-stone-700  dark:border-stone-700 rounded-full relative transition-[background] focus-within:outline-none focus-within:shadow-[0_0_0_1px] focus-within:border-stone-800 focus-within:shadow-stone-800',
+    base: 'cursor-pointer w-[32px] h-[20px] shadow-sm flex data-[state=unchecked]:bg-stone-300 data-[state=checked]:bg-stone-800 dark:data-[state=unchecked]:bg-stone-800 dark:data-[state=checked]:bg-stone-700 border border-stone-300 data-[state=checked]:border-stone-700  dark:border-stone-700 rounded-full relative transition-[background] focus-within:outline-none focus-within:shadow-[0_0_0_1px] focus-within:border-stone-800 focus-within:shadow-stone-800',
     label: '',
     loadingIcon: 'shrink-0 text-neutral-500',
     indicator: 'w-3.5 h-3.5 my-auto bg-white text-xs flex items-center justify-center shadow-xl rounded-full transition-transform translate-x-0.5 will-change-transform data-[state=checked]:translate-x-full',
@@ -30,15 +30,12 @@ export const switchTv = tv({
       }
     },
     loading: {
-      true: {
-        icon: 'animate-spin'
-      }
+      true: ''
     },
   },
   compoundVariants: [
     {
       loading: true,
-      leading: true,
       class: {
         loadingIcon: 'animate-spin',
       },
