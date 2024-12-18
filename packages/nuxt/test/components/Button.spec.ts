ed7d453515fc9a35701dfbb7b58742cd121000a0
Jolan <jolan.brizard@gmail.com>
2024-12-18 23:15:57 +0100

    fix import on tests

--- packages/nuxt/test/components/Button.spec.ts
+++ packages/nuxt/test/components/Button.spec.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from 'vitest'
 import { renderSuspended } from '@nuxt/test-utils/runtime'
-import { MButton } from '#components'
+import Button from '../../src/runtime/components/Button.vue'
 
 describe('Button', () => {
   it('can mount component', async () => {
-    const html = await renderSuspended(MButton)
+    const html = await renderSuspended(Button)
     expect(html.html()).toMatchSnapshot()
   })
 })
