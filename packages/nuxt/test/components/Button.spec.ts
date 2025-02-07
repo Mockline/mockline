1943790ae6e368d943576a36f6f3a65ee87b0f67
Jolan <jolan.brizard@gmail.com>
2025-02-07 11:11:02 +0100

    feat: add new test implementation (heavily inspired by NuxtUI)

--- packages/nuxt/test/components/Button.spec.ts
+++ packages/nuxt/test/components/Button.spec.ts
@@ -1,10 +1,21 @@
 import { describe, it, expect } from 'vitest'
-import { renderSuspended } from '@nuxt/test-utils/runtime'
-import Button from '../../src/runtime/components/Button.vue'
+import ComponentRender from '../component-render'
+import { MButton } from '#components'
 
 describe('Button', () => {
-  it('can mount component', async () => {
-    const html = await renderSuspended(Button)
-    expect(html.html()).toMatchSnapshot()
+  it.each([
+    ['basic case', { }],
+    ['leading icon', { props: { leading: true, icon: 'heroicons-check' } }],
+    ['black solid', { props: { color: 'black', variant: 'solid' } }],
+    ['rounded full', { props: { ui: { rounded: 'rounded-full' } } }],
+    ['<UButton icon="i-heroicons-pencil-square" size="sm" color="primary" square variant="solid" />']
+    // @ts-expect-error
+  ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof UButton.props>) => {
+    if (options !== undefined) {
+      options.slots = options.slots || { default: () => 'label' }
+      options.slots.default = options.slots.default || (() => 'label')
+    }
+    const html = await ComponentRender(nameOrHtml, options, MButton)
+    expect(html).toMatchSnapshot()
   })
 })
