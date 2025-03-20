0bb6b9be38bcf7dd7250fbf8d94780fbca2fa19e
HugoRCD <hugo.richard@epitech.eu>
2025-03-20 10:31:18 +0100

    up

--- packages/nuxt/src/plugins/components.ts
+++ packages/nuxt/src/plugins/components.ts
@@ -38,6 +38,7 @@ export default function ComponentImportPlugin(options: MocklineOptions, meta: Un
     ],
     resolvers: [
       (componentName) => {
+        console.log('componentName', componentName)
         if (overrideNames.has(componentName)) {
           const realName = removeMPrefix(componentName)
           return {
@@ -68,6 +69,7 @@ export default function ComponentImportPlugin(options: MocklineOptions, meta: Un
           return
 
         const filename = id.match(/([^/]+)\.vue$/)?.[1] ?? ''
+        console.log('filename', filename)
         if (overrideNames.has('M' + filename) || overrideNames.has(filename)) {
           return join(runtimeDir, 'vue/components', filename + '.vue')
         }
