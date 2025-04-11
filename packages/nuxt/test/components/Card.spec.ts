aef014eacf0a2142430da249a75e517618271885
johann <johann.cavallucci@epitech.eu>
2025-04-11 10:54:39 +0200

    feat: update card component tests to include new variants and custom class

--- packages/nuxt/test/components/Card.spec.ts
+++ packages/nuxt/test/components/Card.spec.ts
@@ -1,12 +1,16 @@
 import { describe, it, expect } from 'vitest'
-import type {TypeOf} from "zod";
+import type { TypeOf } from "zod";
 // @ts-ignore
-import {MCard} from "#components";
+import { MCard } from "#components";
 import ComponentRender from "../component-render";
 
 describe('Card', () => {
   it.each([
-    ['base case', {}]
+    ['base case (outline by default)', {}],
+    ['with outline variant', { variant: 'outline' }],
+    ['with soft variant', { variant: 'soft' }],
+    ['with subtle variant', { variant: 'subtle' }],
+    ['with custom class', { class: 'custom-class' }]
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof MCard.props>) => {
     const html = await ComponentRender(nameOrHtml, options, MCard)
     expect(html).toMatchSnapshot()
