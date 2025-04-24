55ba89e7bb1d4e890995c39ee83e9cb7aeb9af38
johann <johann.cavallucci@epitech.eu>
2025-04-24 16:37:26 +0200

    feat: enhance PageMarquee component with additional instances and layout options

--- packages/nuxt/playground/app/pages/playground/page-marquee.vue
+++ packages/nuxt/playground/app/pages/playground/page-marquee.vue
@@ -3,8 +3,26 @@
 
 <template>
   <MPageBody>
-    <div>
+    <div class="flex flex-col gap-10">
       <MPageMarquee>
+        <Icon name="i-simple-icons-github" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-discord" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-x" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-instagram" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-linkedin" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-facebook" class="size-10 shrink-0" />
+      </MPageMarquee>
+
+      <MPageMarquee reverse>
+        <Icon name="i-simple-icons-github" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-discord" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-x" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-instagram" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-linkedin" class="size-10 shrink-0" />
+        <Icon name="i-simple-icons-facebook" class="size-10 shrink-0" />
+      </MPageMarquee>
+
+      <MPageMarquee reverse orientation="vertical">
         <Icon name="i-simple-icons-github" class="size-10 shrink-0" />
         <Icon name="i-simple-icons-discord" class="size-10 shrink-0" />
         <Icon name="i-simple-icons-x" class="size-10 shrink-0" />
